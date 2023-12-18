f69179c1deac49b4aef727c100f1365b3735c6e0
aidanho1188 <aaron23657@gmail.com>
2023-12-18 10:00:54 -0500

    add setPuzzleIsSolved function

--- Puzzle.js
+++ Puzzle.js
@@ -1,9 +1,13 @@
 class Puzzle {
-    constructor(puzzleName, puzzleLocation, puzzleMessage, puzzleSolvedMessage ){
+    // puzzles[0].name, puzzles[0].location, puzzles[0].puzzleMessage, puzzles[0].puzzleSolved, puzzles[0].puzzleAnswer, puzzles[0].puzzleWrongAnswer, puzzles[0].puzzleIsSolved);
+    constructor(puzzleName, puzzleLocation, puzzleMessage, puzzleSolvedMessage, puzzleAnswer, puzzleWrongAnswer , puzzleIsSolved){
         this._puzzleName = puzzleName;
         this._puzzleLocation = puzzleLocation;
         this._puzzleMessage = puzzleMessage;
         this._puzzleSolvedMessage = puzzleSolvedMessage;
+        this._puzzleAnswer = puzzleAnswer;
+        this._puzzleWrongAnswer = puzzleWrongAnswer;
+        this._puzzleIsSolved = false;
     }
 
     get puzzleName(){
@@ -20,6 +24,18 @@ class Puzzle {
         
     get puzzleSolvedMessage(){
         return this._puzzleSolvedMessage;
+    }
+
+    get puzzleAnswer(){
+        return this._puzzleAnswer;
+    }
+
+    get puzzleWrongAnswer(){
+        return this._puzzleWrongAnswer;
+    }
+
+    get isSolved(){
+        return this._isSolved;
     }
 }
 exports.Puzzle = Puzzle;
--- index.js
+++ index.js
@@ -21,16 +21,17 @@ function ask(questionText) {
 
 // hmm its so dry here 
 // TODO: fix this and put all classes into a folder
-const startRoom = new Location(rooms[0].name, rooms[0].description, rooms[0].inventory, true,);
+const startRoom = new Location(rooms[0].name, rooms[0].description, rooms[0].inventory, true);
 const room1 = new Location(rooms[1].name, rooms[1].description, rooms[1].inventory, false);
 const room2 = new Location(rooms[2].name, rooms[2].description, rooms[2].inventory, false);
 const room3 = new Location(rooms[3].name, rooms[3].description, rooms[3].inventory, false);
 const room4 = new Location(rooms[4].name, rooms[4].description, rooms[4].inventory, false);
+const room5 = new Location(rooms[5].name, rooms[5].description, rooms[5].inventory, false);
 
 const sign = new Item(items[0].name, items[0].description, items[0].location, items[0].isTakeable);
 const paper = new Item(items[1].name, items[1].description, items[1].location, items[1].isTakeable);
 
-const lockpad = new Puzzle(puzzles[0].name, puzzles[0].location, puzzles[0].puzzleMessage, puzzles[0].puzzleSolved);
+const lockpad = new Puzzle(puzzles[0].name, puzzles[0].location, puzzles[0].puzzleMessage, puzzles[0].puzzleSolved, puzzles[0].puzzleAnswer, puzzles[0].puzzleWrongAnswer, puzzles[0].puzzleIsSolved);
 const player = new Player();
 
 let itemLookUp = {
@@ -76,15 +77,16 @@ let locationState = {
 };
 
 let roomNameLookup = {
-  startRoom: "street",
-  room1: "church",
-  room2: "floor1",
-  room3: "floor2",
-  room4: "floor3",
+  startRoom: ["street", "outside"],
+  room1: ["church"],
+  room2: ["floor1", "floor one", "first floor"],
+  room3: ["floor2", "floor two", "second floor"],
+  room4: ["basement1", "basement one", "first basement"],
+  room5: ["floor3", "basement two", "second basement"],
 };
 
 let puzzleLocation = {
-  church: lockpad
+  room1: lockpad,
 };
 
 start();
@@ -98,9 +100,9 @@ async function start() {
 
 async function gameLoop(player, answer = "") {
   do {
-    answer = await prompt();
+    let answer = await prompt();
     await interact(answer);
-    console.log(`\n${displayRoom(locationLookUp[player.location])}`);
+    console.log(`\n\n\n${displayRoom(locationLookUp[player.location])}`);
   } while (answer !== "exit");
 }
 
@@ -108,14 +110,13 @@ async function prompt(question = "") {
   return ask(`${question}>_ `);
 }
 
-// fix this where player enter only one word
 function getCommand(input) {
   return input[0].toLowerCase();
 }
 
 function getTarget(input) {
   if (input.length > 1) {
-    return input[1];
+    return input.splice(1).join(" ");
   } else {
     return null;
   }
@@ -123,9 +124,9 @@ function getTarget(input) {
 
 async function interact(answer) {
   let answerArr = answer.trim().split(" ");
-  // make sure to make these 2 more flexible with user input
   let command = getCommand(answerArr);
   let target = getTarget(answerArr);
+  console.log(target);
   let commandKey = Object.keys(commandLookUp).find((key) => commandLookUp[key].includes(command)); 
   let commandFunction = commandFunctionLookUp[commandKey];
   // TODO: test for exit church command
@@ -142,43 +143,41 @@ function endGame() {
 }
 
 async function moveRoom(targetedRoom) {
-  let currentRoom = player.location;
-  // console.log(targetedRoom);
-  // console.log(locationLookUp[targetedRoom]);
+  let currentRoom = player._location;
   targetedRoom = getRoomObjectName(targetedRoom);
+  console.log(targetedRoom)
   try {
     let isUnLocked = locationLookUp[targetedRoom]._isUnlocked;
     if (locationState[currentRoom].includes(targetedRoom) && isUnLocked) {
       player._location = targetedRoom;
     } else if (isUnLocked === false) {
-      console.log("Please solve this puzzle");
+      console.log("\n\n\nPlease solve this puzzle");
       await displayRoomPuzzle(targetedRoom);
     }
   } catch (error) {
-    console.log("You can't move to this room!");
+    console.log("You can't move to this room!", error);
   }
 }
 
 async function displayRoomPuzzle(targetedRoom) {
-  let puzzle = puzzleLocation[roomNameLookup[targetedRoom]]
+  let puzzle = puzzleLocation[targetedRoom]
   console.log(`${puzzle._puzzleMessage}`);
   // prompt for password
   while (true) {
-    answer = await prompt("What is the passcode? ");
-    if (answer === "12345") {
-      locationLookUp[targetedRoom]._isUnlocked = true;
-      console.log(`${puzzle._puzzleSolvedMessage}`);
+    let answer = await prompt("What is the passcode? ");
+    if (answer === puzzle._puzzleAnswer) {
+      setPuzzleIsSolved(puzzle, targetedRoom);
       return true;
     } else if (answer === "back") {
       return false;
     } else {
-      console.log("Incorrect passcode, please try again or type \"back\" to exit.");
+      console.log(puzzle._puzzleWrongAnswer);
     }
   }
 }
 
 function getRoomObjectName(targetedRoom) {
-  return Object.keys(roomNameLookup).find((key) => roomNameLookup[key] === targetedRoom);
+  return Object.keys(roomNameLookup).find((key) => roomNameLookup[key].includes(targetedRoom));
 }
 
 function read(item) {
@@ -190,13 +189,20 @@ function read(item) {
   }
 }
 
-function use(item) {
-  // if item is usuable???
+function setPuzzleIsSolved(puzzle, targetedRoom) {
+  locationLookUp[targetedRoom]._isUnlocked = true;
+  puzzle._puzzleIsSolved = true;
+  console.log(`${puzzle._puzzleSolvedMessage}`);
+}
+
+function use(item, target) {
   if (itemLookUp.hasOwnProperty(item)) {
-    // in order to use this item, there must be two objects with present in this room
-    // lookup table or lookup state machine?
+    // item will unlock puzzle
+    if (item === puzzle._puzzleAnswer) {
+      setPuzzleIsSolved(puzzle, targetedRoom)
+    }
   } else {
-    console.log(`You can't use this ${item} 😔`);
+    console.log(`You can't use this ${item} here 😔`);
   }
   return;
 }
@@ -209,7 +215,7 @@ function drop(item) {
     addItemToRoom(player._location, item);
     return console.log(`You dropped ${item} 🤚`);
   } else {
-    return console.log(`You can't take ${item}`);
+    return console.log(`You can't drop this ${item}`);
   }
 }
 
@@ -218,6 +224,8 @@ function take(item) {
     player._inventory.push(item);
     removeItemFromRoom(player._location, item);
     return console.log(`You take ${item} 🤚`);
+  } else if (item === null){
+    return console.log(`You can't take nothing 🚫`);
   } else {
     return console.log(`You can't take this ${item} 🚫`);
   }
@@ -243,7 +251,7 @@ function showPlayerInventory() {
 }
 
 function displayRoom(room) {
-  return `Room name: ${room.name}\nAvailable items: ${room.inventory}\nDescription: "${locationLookUp[player._location].description}"`;
+  return `Room name: ${room.name}   Available items: ${room.inventory}\nDescription: "${locationLookUp[player._location].description}"`;
 }
 
 // TODO: Create More Rooms
