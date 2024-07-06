42b7a4326aeb3782f9128634752918b0853e3e50
aidanho1188 <aaron23657@gmail.com>
2024-07-06 15:57:59 -0400

    chore: Update import statement for Item class in itemsLookUp.js

--- Classes/Item.js
+++ Classes/Item.js
@@ -1,30 +1,31 @@
 class Item {
   constructor(name, description, location, isTakeable, puzzleCode = null) {
-    this._name = name;
-    this._description = description;
-    this._location = location;
-    this._isTakeable = isTakeable;
-    this._puzzleCode = puzzleCode;
+    this._name = name
+    this._description = description
+    this._location = location
+    this._isTakeable = isTakeable
+    this._puzzleCode = puzzleCode
   }
 
   get name() {
-    return this._name;
+    return this._name
   }
 
   get description() {
-    return this._description;
+    return this._description
   }
 
   get location() {
-    return this._location;
+    return this._location
   }
 
   get isTakeable() {
-    return this._isTakeable;
+    return this._isTakeable
   }
 
   get puzzleCode() {
-    return this._puzzleCode;
+    return this._puzzleCode
   }
 }
-exports.Item = Item;
+
+export default Item
